6a1ecdea304709f732fe4a0508184d8ddb7bfc4f
dani91cc <dani91cc@gmail.com>
2015-09-14 20:14:01 +0200

    segundo

--- app/src/main/java/dani/ejemplo/MainActivity.java
+++ app/src/main/java/dani/ejemplo/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //esto es un comentario
+        //otro comentario!!!
     }
 
     @Override
